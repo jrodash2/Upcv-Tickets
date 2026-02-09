f6588e8286d7a362318a2fbb9df13c1859bddae1
jrodash2 <61074808+jrodash2@users.noreply.github.com>
2026-02-09 11:49:04 -0600

    'django.contrib.humanize',
    faltaba en el codigo

--- upcv_app/upcv_app/settings.py
+++ upcv_app/upcv_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'empleados_app',
     'tickets_app.apps.TicketsAppConfig',
     'diplomas_app',
